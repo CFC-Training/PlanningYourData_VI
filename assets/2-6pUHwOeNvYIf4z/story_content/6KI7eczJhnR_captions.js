e36e27c973ad17cb8877161e091407a4315ab825
CFC-Training <107180882+CFC-Training@users.noreply.github.com>
2024-11-07 12:17:27 +0000

    PlanningYourData_VI

---
+++ assets/2-6pUHwOeNvYIf4z/story_content/6KI7eczJhnR_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.85.31840.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.056%0D%0AHave%20you%20ever%20faced%20a%20problem%20like%20this%20before?%0D%0A%0D%0A"}')
